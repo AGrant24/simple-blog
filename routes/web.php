8b92d2c5fb0f3aaa33546a657b099c2b9e9d3ee6
AGrant24 <alex.grant.contact@gmail.com>
2022-06-23 20:59:01 +0100

    index function for articles + displayed Articles

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('index');
+// })->name('home');
 Route::get('/', function () {
-    return view('index');
-})->name('home');
+    return view('articles.index');
+});
+Route::resource('articles', \App\Http\Controllers\ArticleController::class);
 
 Route::view('/about', 'about')->name('about');
 
